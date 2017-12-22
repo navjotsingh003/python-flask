b0a2be56d901ec6b0a532af4bb2fc41e8ff29b7a
navjotsingh003 <navjotsingh003@gmail.com>
2017-12-22 15:20:42 -0800

    Update server.py

--- server.py
+++ server.py
@@ -1,76 +1,101 @@
-from pymysql import connect, cursors
-import db
+from pymysql import connect, cursors, Error
+from flask import Flask, g
+from db import mysql_connection
 
-def create_connection():
-    connection = connect(host='localhost',
-                             user='root',
-                             password='',
-                             db='database007',
-                             charset='utf8mb4',
-                             cursorclass=cursors.DictCursor)
-    return connection
+app = Flask(__name__)
 
+@app.route('/checktable/<string:tablename>')
 def checkTableExists(tablename):
-    connection = create_connection()
-    cursor = connection.cursor()
-    stmt = "SHOW TABLES LIKE '%s'"%(tablename)
-    cursor.execute(stmt)
-    print(cursor.fetchone())
-    a=cursor.fetchone()
-    if a:
+    try:
+        connection = mysql_connection.create_connection()
+        cursor = connection.cursor()
+        stmt = "SHOW TABLES LIKE '%s'"%(tablename)
+        cursor.execute(stmt)
+        print(cursor.fetchone())
+        a=cursor.fetchone()
+        if a:
+            cursor.close()
+            return True
         cursor.close()
-        return True
-    cursor.close()
-    return False
+        return False
+    except (Error, Exception) as e:
+        print("Error occured %s"%e)
 
+@app.route('/')
+def welcome():
+    return "Welcome"
+
+@app.route("/create/<string:table_name>" ,methods = ["GET","POST"])
 def create_table(table_name):
-    connection = create_connection()
-    cursor = connection.cursor()
-    sql_statement = """Create Table %s (ID int auto_increment Primary Key, lastname VARCHAR(255), age int,
-                    email VARCHAR(255))""" %(table_name)
-    cursor.execute(sql_statement)
-    connection.commit()
+    try:
+        connection = mysql_connection.create_connection()
+        cursor = connection.cursor()
+        sql_statement = """Create Table %s (ID int auto_increment Primary Key, lastname VARCHAR(255), age int,
+                        email VARCHAR(255))""" %(table_name)
+        cursor.execute(sql_statement)
+        connection.commit()
+        data ={"table": "%s created" %(table_name)}
+    except (Error, Exception) as e:
+        print("Error occured %s"%e)
+        data = {"Error": e}
+    return data
 
+@app.route("/drop/<string:tableName>")
 def dropTable(tableName):
-    connection = create_connection()
-    cursor = connection.cursor()
-    sql_statement = "Drop Table %s" % (tableName)
-    cursor.execute(sql_statement)
-    connection.commit()
+    try:
+        connection = mysql_connection.create_connection()
+        cursor = connection.cursor()
+        sql_statement = "Drop Table %s" % (tableName)
+        cursor.execute(sql_statement)
+        connection.commit()
+        data = {"table": "%s dropped"%(tableName)}
+    except (Error, Exception) as e:
+        print("Error occured %s"%e)
+        data = {"Error": e}
+    return data
 
-def insert_entry(table_namee,last_name,ages,email_id):
-    connection = create_connection()
-    cursor = connection.cursor()
-    sql_statement = "Insert into %s (lastname,age,email) Values ('%s',%d,'%s')" %(table_namee,last_name,ages,email_id)
-    cursor.execute(sql_statement)
-    connection.commit()
+@app.route("/insert/<string:last_name>/<int:ages>/<string:email_id>")
+def insert_entry(last_name,ages,email_id):
+    try:
+        connection = mysql_connection.create_connection()
+        cursor = connection.cursor()
+        sql_statement = "Insert into %s (lastname,age,email) Values ('%s',%d,'%s')" %(table_namee,last_name,ages,email_id)
+        cursor.execute(sql_statement)
+        connection.commit()
+        data = {"inserted entry": "%s %d %s"%(last_name,ages,email_id)}
+    except (Error, Exception) as e:
+        print("Error occured %s"%e )
+        data = {"Error": e}
+    return data
 
-def delete_entry(table_name,id_no):
-    connection = create_connection()
-    cursor = connection.cursor()
-    sql_staement = "Delete from %s where id= %s"%(table_name,id_no)
-    cursor.execute(sql_staement)
-    connection.commit()
+@app.route("/delete/<int:id_no>")
+def delete_entry(id_no):
+    try:
+        connection = mysql_connection.create_connection()
+        cursor = connection.cursor()
+        sql_staement = "Delete from nav2 where id= %s"%(id_no)
+        cursor.execute(sql_staement)
+        connection.commit()
+        data = {"entry": "%d deleted"%(id_no)}
+    except (Error, Exception) as e:
+        print("Error occured %s"%e)
+        data = {"Error": e}
+    return data
 
-def update_entry(table_name, last_name, ages, email_id, ids):
-    connection = create_connection()
-    cursor = connection.cursor()
-    sql_statement = "UPDATE %s SET lastname= '%s', age = %d, email= '%s' where id = %d" %(table_name, last_name, ages, email_id, ids)
-    cursor.execute(sql_statement)
-    connection.commit()
+@app.route("/update/<string:last_name>/<int:ages>/<string:email_id>/<int:ids>")
+def update_entry(last_name, ages, email_id, ids):
+    try:
+        connection = mysql_connection.create_connection()
+        cursor = connection.cursor()
+        sql_statement = "UPDATE nav2 SET lastname= '%s', age = %d, email= '%s' where id = %d" %(last_name, ages, email_id, ids)
+        cursor.execute(sql_statement)
+        connection.commit()
+        data = {"deleted entryid": "%d"%(ids)}
+    except (Error, Exception) as e:
+        print("Error occured %s"%e)
+        data = {"Error": e}
+    return data
 
-#if __name__ == "__main__":
-#    a = checkTableExists('na')
-#    if a:
-#        print("table alrteady exist")
-#    else:
-#        create_table('fff')
 
-#if __name__ == "__main__":
-#    dropTable('nnnn')
-#if __name__ == "__main__":
-#    insert_entry('nav2', 'Bains', 19, 'bbbbbbb@gmail.com')
-#if __name__ == "__main__":
-#    delete_entry('nav2','4')
-#if __name__ == "__main__":
-#    update_entry('nav2', "Bains", 25, "navjotsingh003@gmail.com", 3)
+if __name__ == "__main__":
+    app.run()
